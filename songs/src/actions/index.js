8bec23dc85e86ff4373b6bb26997eb62cecf074d
gyugyu90 <kh2000park@gmail.com>
2021-07-19 23:28:37 +0900

    integrate redux with song list and song detail

--- songs/src/actions/index.js
+++ songs/src/actions/index.js
@@ -6,4 +6,3 @@ export const selectSong = (song) => {
     payload: song
   };
 };
-
--- songs/src/components/App.js
+++ songs/src/components/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import SongList from './SongList';
-
+import SongDetail from './SongDetail';
 
 const App = () => {
   return (
@@ -9,8 +9,10 @@ const App = () => {
         <div className="column eight wide">
           <SongList />
         </div>
+        <div className="column eight wide">
+          <SongDetail />
+        </div>
       </div>
-      
     </div>
   );
 };
