353f0590c555521b7760203d7370cb83f8708af9
gyugyu90 <kh2000park@gmail.com>
2021-07-29 23:10:01 +0900

    add redux thunk

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,10 +1,10 @@
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
-export const fetchPosts = () => {
-  const response = jsonPlaceholder.get('/posts');
+export const fetchPosts = () => async dispatch => {
+  const response = await jsonPlaceholder.get('/posts');
 
-  return {
+  dispatch({
     type: 'FETCH_POSTS',
     payload: response
-  };
-};
+  })
+}
